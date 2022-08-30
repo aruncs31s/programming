a415f4f3afa3b49b6f4295de909e106e13480083
Arun cs <87601622+aruncs31s@users.noreply.github.com>
2022-08-30 20:38:05 +0530

    Create ifElseShort.js

---
+++ js/ifElseShort.js
@@ -0,0 +1,10 @@
+/*You are given a program that takes the age of the user as input.
+Complete the code to check if the user is an adult, 
+and output to the console the corresponding boolean value.*/
+
+function main() {
+    var age = parseInt(readLine(), 10)
+  var result =
+      age >= 18 ? true : false ;
+    console.log(result )
+}
