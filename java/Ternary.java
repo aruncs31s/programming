ed09c57edc46ea2980477945520ac0e778f39018
ibnurasheed <123855260+ibnurasheed@users.noreply.github.com>
2023-01-29 10:55:01 +0530

    Add files via upload

---
+++ java/Ternary.java
@@ -0,0 +1,9 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int a=7;
+       int  b=(a==2) ? 9: 13;
+        System.out.println(b);
+        b=(a==7) ? 11: 15;
+        System.out.println(b);
+    }
+}
