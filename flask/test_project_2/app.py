7085ba6d9c80da1601219c926e1989800cde24b5
aruncs31s <aruncs31ss@gmail.com>
2023-03-07 22:31:48 +0530

    added new project in flask/

---
+++ flask/test_project_2/app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/<name>')
+def names(name):
+    return render_template('User.html')
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
