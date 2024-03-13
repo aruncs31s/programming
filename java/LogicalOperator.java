41bf28af2f34796b7fdccb7a9c7c37ab13b9467f
aruncs31s <aruncs31ss@gmail.com>
2024-03-13 20:53:48 +0530

    upload *.*

--- java/LogicalOperator.java
+++ java/LogicalOperator.java
@@ -1,9 +1,9 @@
 public class LogicalOperator {
-    public static void main(String[] args) {
-        boolean a=true;
-        boolean b=false;
-        System.out.println(a&&b);
-        System.out.println(a||b);
-        System.out.println(!(a&&b));
-    }
+  public static void main(String[] args) {
+    boolean a = true;
+    boolean b = false;
+    System.out.println(a && b);
+    System.out.println(a || b);
+    System.out.println(!(a && b));
+  }
 }
