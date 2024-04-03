cef96e8eb5fc53a78c07b839e0906e96b9c675ec
aruncs31s <aruncs31ss@gmail.com>
2024-04-03 18:03:30 +0530

    update c programming activities

---
+++ c/Basics/Activities/task_2.c
@@ -0,0 +1,13 @@
+// Task : Calculate the area of the rectangle and store the result in result variable
+// Hint : use printf to print the result
+
+int main()
+{
+  int length_1 = 10;// Length of one opposite side
+  int length_2 = 20;// Length of the adjesent side
+  int result ;
+  //your code goes here
+
+  return 0;
+}
+
