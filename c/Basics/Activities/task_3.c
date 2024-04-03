fd84c0f4215d739ddb91996a1712d2fe460e8375
aruncs31s <aruncs31ss@gmail.com>
2024-04-03 18:21:09 +0530

    update c programming activities

---
+++ c/Basics/Activities/task_3.c
@@ -0,0 +1,12 @@
+// Task : Calculate the value of x if x = 2x^2+ 2x + 4 ; ^ indicates the power operator or power
+// Hints :
+// use printf to print the result
+// use sqrt() function in math to calculate the square root of a function
+
+int main()
+{
+   //your code goes here
+
+  return 0;
+}
+
