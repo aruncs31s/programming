8aa08b98c6543994b96a586dc4896121414ce2e4
aruncs31s <aruncs31ss@gmail.com>
2024-04-08 18:29:11 +0530

    new file:   ../../Machine Learning/p4.py modified:   README.md

---
+++ Machine Learning/p4.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+inputs_1 = [1, 2, 3, 4]
+inputs_2 = [1, 3, 4, 5]
+inputs_3 = [1.1, 2.2, 3, 4]
+
+weights_1 = [1, 3, 3, 5]
+weights_2 = [0.2, 3, 3, 3]
+weights_3 = [1.12, 3.2, 3, 3]
+biases = [1, 2, 3]
+
+output = []
+
+output.append = 1
+
+# np.dot(inputs_1,weights_1)
+# + biases[0]
+print(output)
