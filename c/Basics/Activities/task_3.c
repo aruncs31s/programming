5de7d28ecbd4343e6df6ac75f08f475906689154
aruncs31s <aruncs31ss@gmail.com>
2024-06-06 17:09:41 +0530

    Backup

--- c/Basics/Activities/task_3.c
+++ c/Basics/Activities/task_3.c
@@ -1,12 +1,9 @@
-// Task : Calculate the value of x if x = 2x^2+ 2x + 4 ; ^ indicates the power operator or power
-// Hints :
-// use printf to print the result
-// use sqrt() function in math to calculate the square root of a function
+// Task : Print the number 1 to 10
+// Hint : use for loop
+//
 
-int main()
-{
-   //your code goes here
+int main() {
+  // your code goes here
 
   return 0;
 }
-
--- c/Basics/Activities/task_4.c
+++
@@ -1,9 +0,0 @@
-// Task : Print the number 1 to 10
-// Hint : use for loop
-//
-
-int main() {
-  // your code goes here
-
-  return 0;
-}
