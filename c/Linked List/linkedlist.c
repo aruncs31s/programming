72524fda715d62be1aa3abc912b89dc7d96b7910
aruncs31s <aruncs31ss@gmail.com>
2024-09-17 16:02:32 +0530

    new file:   a.out 	new file:   basicLinkedList.c 	modified:   linkedlist.c 	new file:   test.c

--- c/Linked List/linkedlist.c
+++ c/Linked List/linkedlist.c
@@ -1,4 +1,3 @@
-#include <cstddef>
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -15,16 +14,17 @@ typedef struct node_t node_t;
 void print_the_list(node_t *head) {
   node_t *temperory_ptr = head;
   while (temperory_ptr != NULL) {
-    printf("%d", temperory_ptr->value);
+    printf("%d - ", temperory_ptr->value);
     temperory_ptr = temperory_ptr->next;
   }
+  printf("\n");
 }
 int main() {
   node_t n1, n2, n3;
   node_t *head;
-  n1.value = 10;
-  n2.value = 100;
-  n3.value = 200;
+  n1.value = 1;
+  n2.value = 2;
+  n3.value = 3;
 
   // Linking them
   head = &n3;
@@ -32,5 +32,13 @@ int main() {
   n2.next = &n1;
   n1.next = NULL; // tail
 
+  print_the_list(head);
+  // Inserting New Node
+  node_t n4;
+  n4.value = 4;
+  n4.next = &n3;
+  n3.next = &n2;
+  head = &n4;
+  print_the_list(head);
   return 0;
 }
